5ed02759a196a96f3f53b0d85d90cf56b472a50b
Luca Negrette <lucanegrette@hotmail.com>
2019-11-25 01:29:00 -0300

    csacasc

--- homePage.php
+++ homePage.php
@@ -75,8 +75,8 @@ $app->get('/register/confirm', function() {
     
         $datarecovery = openssl_decrypt($data, 'AES-128-CBC', pack("a16", User::SECRET), 0, pack("a16", User::SECRET_IV));
 
-        $data = json_decode($data, true);
-        var_dump($data);
+        $datarecovery  = json_decode($datarecovery , true);
+        var_dump($datarecovery );
         exit;
 
         $user = new User();

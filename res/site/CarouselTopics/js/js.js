067fe9d5bcb686f58ffb71a9d7303cfcae93d5b0
Luca Negrette <lucanegrette@hotmail.com>
2019-11-26 11:09:38 -0300

    csacsa

--- res/site/CarouselTopics/js/js.js
+++ res/site/CarouselTopics/js/js.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function($) {
     "use strict";
     //  TESTIMONIALS CAROUSEL HOOK
-    $('#customers-testimonials').owlCarousel({
+    $('#customers-testimonials1').owlCarousel({
         loop: true,
         center: true,
         items: 3,
@@ -9,6 +9,27 @@ jQuery(document).ready(function($) {
         autoplay: true,
         dots:true,
         autoplayTimeout: 8500,
+        smartSpeed: 450,
+        responsive: {
+          0: {
+            items: 1
+          },
+          768: {
+            items: 2
+          },
+          1170: {
+            items: 3
+          }
+        }
+    });
+    $('#customers-testimonials2').owlCarousel({
+        loop: true,
+        center: true,
+        items: 3,
+        margin: 0,
+        autoplay: true,
+        dots:true,
+        autoplayTimeout: 9500,
         smartSpeed: 450,
         responsive: {
           0: {

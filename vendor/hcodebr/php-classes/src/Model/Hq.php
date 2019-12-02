36c7dd9c1ffb56b5c4defd8c408d7e31a6543f5d
Luca Negrette <lucanegrette@hotmail.com>
2019-12-01 21:19:16 -0300

    csaca

--- vendor/hcodebr/php-classes/src/Model/Hq.php
+++ vendor/hcodebr/php-classes/src/Model/Hq.php
@@ -97,6 +97,56 @@ class Hq extends Model
 			":idhq"=>$this->getidhq()
 		]);
 	}
+	public static function getPage($page = 1, $itemsPerPage = 10)
+	{
+		
+		$start = ($page - 1) * $itemsPerPage;
+
+		$sql = new Sql();
+		$results = $sql->select("
+		
+			SELECT SQL_CALC_FOUND_ROWS *
+			FROM tb_hqs 
+			ORDER BY deshq
+			LIMIT $start, $itemsPerPage;
+		");
+
+		$resultTotal = $sql->select("SELECT FOUND_ROWS() AS nrtotal;");
+
+		return [
+			'data'=>$results,
+			'total'=>(int)$resultTotal[0]["nrtotal"],
+			'pages'=>ceil($resultTotal[0]["nrtotal"] / $itemsPerPage)
+		];
+
+	}
+	public static function getPageSearch($search, $page = 1, $itemsPerPage = 10) // LIKE = como ou mais ou menos igual
+																				 //  = exatamente igual ao especificado 
+	{
+		
+		$start = ($page - 1) * $itemsPerPage;
+
+		$sql = new Sql();
+		$results = $sql->select("
+		
+			SELECT SQL_CALC_FOUND_ROWS *
+			FROM tb_hqs 
+			WHERE deshq LIKE :search  
+			ORDER BY deshq 
+			LIMIT $start, $itemsPerPage;
+		", [
+			":search"=>"%".$search."%"
+		]);
+
+		$resultTotal = $sql->select("SELECT FOUND_ROWS() AS nrtotal;");
+
+		return [
+			'data'=>$results,
+			'total'=>(int)$resultTotal[0]["nrtotal"],
+			'pages'=>ceil($resultTotal[0]["nrtotal"] / $itemsPerPage)
+		];
+
+	}
 	
 }
 
